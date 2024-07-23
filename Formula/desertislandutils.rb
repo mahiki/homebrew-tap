a8d10fcad132a86584f0b1a1ad4ba9edb65176ba
mahiki <mahiki@users.noreply.github.com>
2024-07-24 00:03:24 +0200

    formula finally good on v0.3.9

--- Formula/desertislandutils.rb
+++ Formula/desertislandutils.rb
@@ -12,33 +12,13 @@ class Desertislandutils < Formula
 
   def install
     venv = virtualenv_create(libexec, "python3")
-
     ENV["VIRTUAL_ENV"] = libexec
-
-    puts "libexec: #{libexec}"
-        # this comes as: libexec: /opt/homebrew/Cellar/desertislandutils/0.3.10/libexec
-
     system "poetry", "install", "--no-root"
-
     venv.pip_install_and_link buildpath
-
   end
 
   test do
-    # TODO: you should like test the brew install and function test of your cli things
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test desertislandutils`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "#{bin}/wn", "--help"
-
-    # TODO: basic test of built function
-    # system "#{bin}/wn", "--help"
-    # system "#{bin}/too", "--help"
+    system "#{bin}/too", "--help"
   end
 end
