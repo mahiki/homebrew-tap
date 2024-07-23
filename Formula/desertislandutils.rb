e1b47645d0c8963e164701a0b3844c2bfc4e691e
mahiki <mahiki@users.noreply.github.com>
2024-07-24 00:59:07 +0200

    tweaks to path. it works locally

--- Formula/desertislandutils.rb
+++ Formula/desertislandutils.rb
@@ -3,8 +3,8 @@ class Desertislandutils < Formula
 
   desc "Be here, thy collection of personal convenience utilities"
   homepage "https://github.com/mahiki/homebrew-tap"
-  url "https://github.com/mahiki/desertislandutils/releases/download/v0.3.10/desertislandutils-0.3.10-py3-none-any.whl"
-  sha256 "ee2a20015a6329e3a298ffd7a562e519161599331c0424c99c4c77afa3af68c3"
+  url "https://github.com/mahiki/desertislandutils/releases/download/v0.3.10/desertislandutils-0.3.10.tar.gz"
+  sha256 "9c8304fdb02a1fd9d3ffd460cdaf25f1b22a70f2d18ebde700080d328c306216"
   license "MIT"
 
   depends_on "python@3.11"
@@ -13,9 +13,10 @@ class Desertislandutils < Formula
   def install
     venv = virtualenv_create(libexec, "python3")
     ENV["VIRTUAL_ENV"] = libexec
-    ENV.prepend_path "PATH", "#{libexec_path}/bin"
-    puts "instal stage libexec path: #{libexec}"
-    puts "env VIRTUAL_ENV: #{libexec}"
+    ENV.prepend_path "PATH", "#{libexec}/bin"
+    puts "install stage libexec path: #{libexec}"
+    puts "env VIRTUAL_ENV: #{ENV["VIRTUAL_ENV"]}"
+    puts "buildpath: #{buildpath}"
     cd buildpath do
         system "poetry", "install", "--no-root"
     end
